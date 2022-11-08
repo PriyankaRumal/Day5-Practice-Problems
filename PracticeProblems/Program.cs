ccd563aefbdb88d70b0dd0a02e8b846d79dd8543
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-08 15:39:51 -0800

    Solution added

---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PracticeProblems
+{
+    internal class Program
+    {
+        static int hco = 0;
+        static int tco = 0;
+
+        static void Main(string[] args)
+        {
+            FlipCoin(4);
+        }
+        public static void FlipCoin(int n)
+        {
+            int temp = n;
+            Random rnd = new Random();
+            while (n != 0)
+            {
+                double random = rnd.NextDouble();
+                if (random <= 0.5)
+                {
+                    tco = tco + 1;
+                }
+                else
+                {
+                    hco++;
+                }
+                n = n - 1;
+            }
+            Console.WriteLine((hco * 100) / temp);
+            Console.WriteLine((tco * 100) / temp);
+
+
+        }
+    }
+}
